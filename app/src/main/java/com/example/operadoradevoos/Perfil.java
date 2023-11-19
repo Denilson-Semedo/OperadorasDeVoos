025552312e01156e9974af3d101f033e1d2360da
Denilson Tavares <dstavares00@gmail.com>
2023-11-19 01:28:31 -0100

    Tela de perfil

--- app/src/main/java/com/example/operadoradevoos/Perfil.java
+++ app/src/main/java/com/example/operadoradevoos/Perfil.java
@@ -1,13 +1,25 @@
 package com.example.operadoradevoos;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Perfil#newInstance} factory method to
@@ -23,6 +35,15 @@ public class Perfil extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnEditar;
+
+    private EditText nome;
+    private EditText email;
+    private EditText identificacao;
+    private EditText dataNascimento;
+
+
 
     public Perfil() {
         // Required empty public constructor
@@ -55,10 +76,69 @@ public class Perfil extends Fragment {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        btnEditar = (Button) view.findViewById(R.id.btnEditar);
+        nome = (EditText) view.findViewById(R.id.nome);
+        email = (EditText) view.findViewById(R.id.email);
+        identificacao = (EditText) view.findViewById(R.id.identificacao);
+        dataNascimento = (EditText) view.findViewById(R.id.dataNasc);
+
+        getDataFromFirebaseRealtimeDatabase();
+
+        return view;
+    }
+
+    public void getDataFromFirebaseRealtimeDatabase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://ilhasairlines-default-rtdb.firebaseio.com");
+        DatabaseReference myRef = database.getReference("Utilizadores");
+
+        // get User with id 449
+        Query query = myRef.orderByChild("uid").equalTo("tizFkzZ8ANSk9OlXLBxCcBwCHNA3");
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String nomeValue = "";
+                    String emailValue = "";
+                    String identificacaoValue = "";
+                    String dataNascimentoValue = "";
+
+                    // Check if the child exists before retrieving its value
+                    if (dataSnapshot.child("nome").exists()) {
+                        nomeValue = dataSnapshot.child("nome").getValue().toString();
+                    }
+
+                    if (dataSnapshot.child("email").exists()) {
+                        emailValue = dataSnapshot.child("email").getValue().toString();
+                    }
+
+                    if (dataSnapshot.child("identificação").exists()) {
+                        identificacaoValue = dataSnapshot.child("identificação").getValue().toString();
+                    }
+
+                    if (dataSnapshot.child("dataNascimento").exists()) {
+                        // Assuming dataNascimento is a Long, convert it to a String
+                        dataNascimentoValue = String.valueOf(dataSnapshot.child("dataNascimento").getValue());
+                    }
+
+                    nome.setText(nomeValue);
+                    email.setText(emailValue);
+                    identificacao.setText(identificacaoValue);
+                    dataNascimento.setText(dataNascimentoValue);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
